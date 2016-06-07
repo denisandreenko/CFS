70fd7976f8468d2b6171b48c7a9ba0ea0d71e317
Andreenko Denis <bountydarus@gmail.com>
2016-06-07 22:20:32 +0300

    description mediaSource

--- src/com/exposit_ds/www/desctiptionMedia/MediaResourse.java
+++ src/com/exposit_ds/www/desctiptionMedia/MediaResourse.java
@@ -1,5 +1,7 @@
 package com.exposit_ds.www.desctiptionMedia;
 
+import com.exposit_ds.www.collections.TypeMedia;
+
 /**
  * Created by knigi on 07.06.2016.
  */
@@ -7,25 +9,46 @@ public class MediaResourse {
 
     private String name;
     private String year;
-    private String nameActor;
+    private String nameAuthor;
+    private TypeMedia type;
 
-    public MediaResourse(String nameMovie, String year, String nameActor)
+
+    public MediaResourse(String nameMovie, String year, String nameAuthor)
     {
         this.name = nameMovie;
         this.year = year;
-        this.nameActor = nameActor;
+        this.nameAuthor = nameAuthor;
     }
 
-    public String getNameMovie() {
+    public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getYear() {
         return year;
     }
 
-    public String getNameActor() {
-        return nameActor;
+    public void setYear(String year) {
+        this.year = year;
     }
 
+    public String getNameActor() {
+        return nameAuthor;
+    }
+
+    public void setNameActor(String nameActor) {
+        this.nameAuthor = nameActor;
+    }
+
+    public TypeMedia getType() {
+        return type;
+    }
+
+    public void setType(TypeMedia type) {
+        this.type = type;
+    }
 }
