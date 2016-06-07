e7f52e3bc0ead0615e80faa266e1ee3aa6bf3f27
Andreenko Denis <bountydarus@gmail.com>
2016-06-07 22:20:11 +0300

    contein mediaSource

--- src/com/exposit_ds/www/collections/AbstractCollection.java
+++ src/com/exposit_ds/www/collections/AbstractCollection.java
@@ -1,7 +1,17 @@
 package com.exposit_ds.www.collections;
 
-/**
- * Created by knigi on 07.06.2016.
- */
-public class AbstractCollection {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AbstractCollection<T> implements Serializable{
+
+    private List<T> listMedia = new ArrayList<>();
+
+    public void add(T media) {
+        listMedia.add(media);
+    }
+
 }
+
