83d2a8a70fd316cf91a18f365a7bdde207976f9b
Andreenko Denis <bountydarus@gmail.com>
2016-06-19 21:52:00 +0300

    rename

--- src/main/java/com/exposit_ds/www/Main.java
+++ src/main/java/com/exposit_ds/www/Main.java
@@ -2,9 +2,9 @@ package com.exposit_ds.www;
 
 import java.io.IOException;
 
-public class RunMedia {
+public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        
+
         PlayMediaCatalog.playMediaCatalog();
     }
 }
